e90f24dfc8c11ed0cdeae23a6fc1e5e4593e1a6f
AbelVolpi <abelvolpi@gmail.com>
2023-06-26 23:13:28 -0300

    Initial commit

---
+++ demo/src/main/kotlin/com/example/demo/Message.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("messages")
+data class Message(
+    @Id val id: String?,
+    @Column("text")
+    val text: String
+)
